db3d13e6f87d4cf9776068f192b92de32e159fd6
Asrar Ahammad <asrarahammadshaik@gmail.com>
2025-12-23 19:12:49 +0530

    Day-2

---
+++ 06_filter_function.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function (arr, fn) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    let value = fn(arr[i], i);
+    if (value) {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+};
