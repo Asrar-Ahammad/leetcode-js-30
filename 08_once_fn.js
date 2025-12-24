1705363dac6cece3de75ab6e49b4fdf12a52c809
Asrar Ahammad <asrarahammadshaik@gmail.com>
2025-12-24 22:09:45 +0530

    Day-3

--- 08_once_fn.js
+++ 08_once_fn.js
@@ -2,11 +2,19 @@
  * @param {Function} fn
  * @return {Function}
  */
-var once = function(fn) {
-    
-    return function(...args){
-        
+var once = function (fn) {
+  let hasBeenCalled = false;
+  let result;
+
+  return function (...args) {
+    if (!hasBeenCalled) {
+      result = fn(...args);
+      hasBeenCalled = true;
+      return result;
+    } else {
+      return undefined;
     }
+  };
 };
 
 /**
